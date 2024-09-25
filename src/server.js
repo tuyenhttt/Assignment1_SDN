1359d3829e3e1ac8a3790df45748a352de98a467
Thanh Tuyen <tuyen13022003@gmail.com>
2024-09-25 23:26:21 +0700

    new commit

--- src/server.js
+++ src/server.js
@@ -1,17 +1,55 @@
+// const express = require("express");
+// const mongoose = require("mongoose");
+// const bodyParser = require("body-parser");
+
+// const app = express();
+
+// // Middleware
+// app.use(bodyParser.json());
+
+// // Connect to MongoDB
+// mongoose.connect("mongodb://localhost:27017/SimpleQuiz", {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// });
+
+// // Routes
+// const quizRoutes = require("./routes/quizRoutes");
+// const questionRoutes = require("./routes/questionRoutes");
+
+// app.get("/", (req, res) => {
+//   res.send("Sample Quizzes");
+// });
+// app.use("/quizzes", quizRoutes);
+// app.use("/questions", questionRoutes);
+
+// // Start server
+// const port = 3000;
+// app.listen(port, () => {
+//   console.log(`Server running at port http://localhost:${port}`);
+// });
+
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const dotenv = require("dotenv");
 
 const app = express();
+
+// Load environment variables
+dotenv.config();
 
 // Middleware
 app.use(bodyParser.json());
 
-// Connect to MongoDB
-mongoose.connect("mongodb://localhost:27017/SimpleQuiz", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+// Connect to MongoDB using MONGO_URI from .env
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB connected successfully"))
+  .catch((err) => console.error("MongoDB connection error:", err));
 
 // Routes
 const quizRoutes = require("./routes/quizRoutes");
@@ -24,7 +62,7 @@ app.use("/quizzes", quizRoutes);
 app.use("/questions", questionRoutes);
 
 // Start server
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server running at port http://localhost:${port}`);
 });
